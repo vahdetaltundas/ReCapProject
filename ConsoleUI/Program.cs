22a602a8db8a6be7af0878ad76f765a31d1c53b2
Vahdet Altundaş <vahdetaltundas2323@gmail.com>
2022-12-12 11:33:56 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,16 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+            carManager.Add(new Car { BrandId = 1, ColorId = 1, DailyPrice = 200, ModelYear = 2010, Description = "Bmw" });
+        }
+    }
+}
